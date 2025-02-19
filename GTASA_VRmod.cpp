53c39d675a5726d72aedf76f55f404020fb5ca01
Holydh <nativemotionsystems@gmail.com>
2025-02-20 00:11:01 +0100

    Cleo removal - Almost done

--- GTASA_VRmod.cpp
+++ GTASA_VRmod.cpp
@@ -38,6 +38,7 @@ private:
 	uevr::API::UObject* playerHead;
 	uevr::API::UObject* weapon;
 	uevr::API::UObject* weaponMesh;
+	uevr::API::UObject* weaponStaticMesh;
 
 	glm::fvec3 crosshairOffset = { 0.0f, -1.0f, 2.0f };
 
@@ -54,6 +55,53 @@ private:
 	glm::fvec3 currentWeaponRecoilRotationEuler = { 0.0f, 0.0f, 0.0f };
 	float recoilPositionRecoverySpeed = 10.0f;
 	float recoilRotationRecoverySpeed = 8.0f;
+
+	std::unordered_map<std::wstring, int> weaponNameToIndex = {
+		{L"SM_unarmed", 0},           // Unarmed
+		{L"SM_brassknuckle", 1},    // BrassKnuckles
+		{L"SM_golfclub", 2},         // GolfClub
+		{L"SM_nightstick", 3},       // NightStick
+		{L"SM_knifecur", 4},            // Knife
+		{L"SM_bat", 5},      // BaseballBat
+		{L"SM_shovel", 6},           // Shovel
+		{L"SM_poolcue", 7},          // PoolCue
+		{L"SM_katana", 8},           // Katana
+		{L"SM_chnsaw", 9},         // Chainsaw
+		{L"SM_gun_dildo1", 10},          // Dildo1
+		{L"SM_gun_dildo2", 11},          // Dildo2
+		{L"SM_gun_vibe1", 12},           // Vibe1
+		{L"SM_gun_vibe2", 13},           // Vibe2
+		{L"SM_flowera", 14},         // Flowers
+		{L"SM_gun_cane", 15},            // Cane
+		{L"SM_grenade", 16},         // Grenade
+		{L"SM_teargas", 17},         // Teargas
+		{L"SM_molotov", 18},         // Molotov
+		{L"SM_colt45", 22},          // Pistol Colt 45
+		{L"SM_silenced", 23},        // Silenced Pistol
+		{L"SM_desert_eagle", 24},     // Desert Eagle
+		{L"SM_chromegun", 25},         // Shotgun
+		{L"SM_sawnoff", 26},         // Sawnoff Shotgun
+		{L"SM_shotgspa", 27},          // Spas12
+		{L"SM_micro_uzi", 28},             // MicroUzi
+		{L"SM_mp5lng", 29},             // MP5
+		{L"SM_ak47", 30},            // AK47
+		{L"SM_m4", 31},              // M4
+		{L"SM_tec9", 32},            // Tec9
+		{L"SM_cuntgun", 33},         // Rifle (Cuntgun)
+		{L"SM_sniper", 34},          // Sniper Rifle
+		{L"SM_rocketla", 35},  // RocketLauncher
+		{L"SM_heatseek", 36},// RocketLauncherHeatSeek
+		{L"SM_flame", 37},    // Flamethrower
+		{L"SM_minigun", 38},         // Minigun
+		{L"SM_satchel", 39},         // Satchel
+		{L"SM_detonator", 40},       // Detonator
+		{L"SM_spraycan", 41},        // SprayCan
+		{L"SM_fire_ex", 42},    // Extinguisher
+		{L"SM_camera", 43},          // Camera
+		{L"SM_nvgoggles", 44},     // NightVision
+		{L"SM_irgoggles", 45},        // Infrared
+		{L"SM_gun_para", 46}        // Parachute
+	};
 
 public:
 	GTASA_VRmod() = default;
@@ -82,7 +130,6 @@ public:
 		characterIsGettingInACar = *(reinterpret_cast<byte*>(memoryManager.characterIsGettingInACarAddress)) > 0;
 		characterIsInCar = *(reinterpret_cast<byte*>(memoryManager.characterIsInCarAddress)) > 0;
 		cameraMode = *(reinterpret_cast<int*>(memoryManager.cameraModeAddress));
-		equippedWeaponIndex =  *(reinterpret_cast<int*>(memoryManager.equippedWeaponAddress));
 		//API::get()->log_info("weaponWheelOpen = %i", weaponWheelOpen);
 
 		//API::get()->log_info("cameraMode = %i",cameraMode);
@@ -771,6 +818,7 @@ public:
 			if (child->is_a(gta_weapon_c)) {
 				weapon = child;
 				weaponMesh = weapon->get_property<API::UObject*>(L"WeaponMesh");
+				weaponStaticMesh = weaponMesh->get_property<API::UObject*>(L"StaticMesh");
 				//API::get()->log_info("%ls", weaponMesh->get_full_name().c_str());
 
 				if (!characterIsInCar || cameraMode == 55)
@@ -791,6 +839,25 @@ public:
 			else
 				weaponMesh = nullptr;
 		}
+
+		if (weaponMesh = nullptr)
+			return;
+
+		
+		std::wstring weaponName = weaponStaticMesh->get_full_name();
+		
+		// Extract only the weapon name from the full path
+		size_t lastDot = weaponName.find_last_of(L'.');
+		if (lastDot != std::wstring::npos) {
+			weaponName = weaponName.substr(lastDot + 1);
+		}
+		API::get()->log_info("%ls", weaponName.c_str());
+		// Look up the weapon in the map
+		auto it = weaponNameToIndex.find(weaponName);
+		if (it != weaponNameToIndex.end()) {
+			equippedWeaponIndex = it->second;
+		}
+		API::get()->log_info("%i", equippedWeaponIndex);
 	}
 
 	glm::fvec3 OffsetLocalPositionFromWorld(glm::fvec3 worldPosition, glm::fvec3 forwardVector, glm::fvec3 upVector, glm::fvec3 rightVector, glm::fvec3 offsets)
